337d9e63cefca950b5afde8c4b8b892bcf61a9ed
Roopika Ponnur <92243098+roopika253@users.noreply.github.com>
2026-07-09 01:38:38 +0530

    Add: search-for-word - submission-1

---
+++ Data Structures & Algorithms/search-for-word/submission-1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def helper(self, r , c , i, j, index , target , board , vis):
+        if i >=r or i < 0 or j >=c or j < 0:
+            return False
+        if vis[i][j] == 0:
+            if target[index] == board[i][j]:
+                index += 1
+                vis[i][j] = 1
+            else :
+                return False
+           
+        else:
+            return False
+        if index == len(target):
+            return True
+        dx = [-1,0,1,0]
+        dy = [0,-1,0,1]
+        for d in range(4):
+            if self.helper(r , c, i+dx[d], j+dy[d] , index , target , board, vis):
+                return True
+        vis[i][j] = 0
+        return False
+       
+           
+        
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        r = len(board)
+        c = len(board[0])
+        vis = [[0]*c for i in range(r)]
+        for i in range(r):
+            for j in range(c):
+                if self.helper(r,c,i,j,0, word , board ,vis):
+                    return True
+        return False
